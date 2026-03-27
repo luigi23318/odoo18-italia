4e714358d4ad01638b041a4b9732a4cda0823e58
luigi23318 <118113843+luigi23318@users.noreply.github.com>
2026-03-27 23:23:59 +0100

    Remove import of account_move_send module

--- l10n_it_edi_pec_bridge/models/__init__.py
+++ l10n_it_edi_pec_bridge/models/__init__.py
@@ -2,6 +2,5 @@
 from . import res_company
 from . import res_config_settings
 from . import account_move
-from . import account_move_send
 from . import sdi_pec_transaction
 from . import pec_mail_handler
